ed2ef1beb10d6fc7dc4d8607e09ec2a4a6bc2ca3
BL7Ki <leoventura245@gmail.com>
2023-06-17 14:36:16 -0300

    img adicionada, pasta img criada e icon alterado

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -67,7 +67,7 @@ function ProfileMenu() {
                         size="sm"
                         alt="candice wu"
                         className="border border-blue-500 p-0.5"
-                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                        src="https://img.a.transfermarkt.technology/portrait/big/50144-1458293316.jpg?lm=1"
                     />
                     <ChevronDownIcon
                         strokeWidth={2.5}
@@ -84,8 +84,8 @@ function ProfileMenu() {
                             key={label}
                             onClick={closeMenu}
                             className={`flex items-center gap-2 rounded ${isLastItem
-                                    ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                                    : ""
+                                ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                                : ""
                                 }`}
                         >
                             {React.createElement(icon, {
